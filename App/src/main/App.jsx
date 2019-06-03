e58f98f062760f054e77878464d38de5e938596a
dayanedias <dayane.doc@hotmail.com>
2019-06-03 02:50:36 -0300

    Ajustes de funcionalidades

---
+++ App/src/main/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
+import './App.css'
+
+import Nav from '../components/template/Nav'
+import Home from '../components/home/Home'
+import Search from '../components/template/Search'
+import Cards from '../components/home/Cards' 
+
+export default props =>
+    <div className="app">
+        
+        <Search />
+        <Nav />
+        <Home />
+        
+    </div>
